68c0eea1d0d5d6094a82c7ce96a809ade9162e3c
Dominique MICHEL <dominique.michel@epita.fr>
2020-08-15 18:00:43 +0200

    ADD dependencies for config_loader

--- src/NGN/__init__.py
+++ src/NGN/__init__.py
@@ -2,6 +2,8 @@ from pymessenger.bot import Bot
 from flask import Flask, request
 from sqlalchemy import Column, String, Integer, ForeignKey, Table
 from sqlalchemy.orm import relationship
+import json
+import inspect
 
 from .app import ACCESS_TOKEN, VERIFY_TOKEN, app, bot
 from .db_manager import Session, engine, Base
@@ -10,5 +12,5 @@ from .flow import handles
 from .routes import verify_fb_token
 
 
-def run():
-    app.run()
+def run(**kwargs):
+    app.run(**kwargs)
