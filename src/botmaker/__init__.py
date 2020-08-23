82c4239d7b2c43b030468de65cfedb4f08331716
Dominique MICHEL <dominique.michel@epita.fr>
2020-08-23 23:37:35 +0200

    FIX removed flask dependency since it has been moved to the user

--- src/botmaker/__init__.py
+++ src/botmaker/__init__.py
@@ -3,10 +3,6 @@ from sqlalchemy import Column, String, Integer, ForeignKey, create_engine,\
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
 
-from flask import Flask, request
-
-#  from .db_manager import Session, engine, Base
-#  from .models import User, Argument
 from .messenger import Messenger
 from .extended_user import ExtendedUser
 from .dispatcher import Dispatcher
