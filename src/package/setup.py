29a2d0cfecad57902deee5de1f93fdf03be2ae4c
Dominique MICHEL <dominique.michel@epita.fr>
2020-08-23 02:26:14 +0200

    ADD setup file to install with pip

---
+++ src/package/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="botmaker-pkg-dominique",
+    version="0.0.1",
+    author="Dominique MICHEL",
+    author_email="dominique.michel@epita.fr",
+    description="This package automates the bot creation process",
+    long_description="See README",
+    long_description_content_type="text/markdown",
+    url="https://github.com/dominique57/NewsGroupNotifier",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
